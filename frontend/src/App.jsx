00ffbf94e5db7d3580a1c9661d9e4c34a33a01d9
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-11-26 11:26:50 +0545

    project setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Index from "./components/header";
+
+const App = () => {
+  return (
+    <>
+      <Index></Index>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/header/Index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <header>
+      <div className="top-strip">
+        <div className="container">
+          <div className="flex item-center justify-between">
+            <div className="col1 w-[50%]">
+              <p>Get up to 50% off new season styles, limited time only</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Index;
